a8b25642567fb871d222cb922ff6ad5f8965dc3c
Joe Ganobsik <jganobsik@gmail.com>
2019-04-22 18:10:21 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,17 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil?
+        redirect_to artists_path, alert: "Artist not found"
+      else
+        @songs = @artist.songs
+      end
+       else
+      @songs = Song.all
+    end
   end
+
 
   def show
     @song = Song.find(params[:id])
